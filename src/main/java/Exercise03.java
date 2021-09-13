85af3ee0cf0fcab4b047d49b6584b13172c405bd
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-12 21:21:11 -0400

    Initial commit

---
+++ src/main/java/Exercise03.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Magdalena Sobrino-Almanzar
+ */
+package exercise;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+
+        System.out.print("What is the quote? ");
+
+        Scanner input = new Scanner(System.in);
+        String quote = input.nextLine();
+
+        System.out.print("Who said it? ");
+        String speaker = input.nextLine();
+
+        System.out.println(speaker + " says, " + "\"" + quote + "\"");
+
+        System.exit(0);
+
+    }//end of main//
+}//end of program//
+
